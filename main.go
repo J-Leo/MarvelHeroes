e43b1ca4a063dbb17435e67631bfae1aa75cd456
unknown <jose.salazar1@unmsm.edu.pe>
2019-02-02 00:56:47 -0500

    Menu(ongoing)

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"crypto/md5"
 	"encoding/hex"
 	"encoding/json"
@@ -43,8 +44,28 @@ func main() {
 	hash.Write([]byte(par))
 	myHash := hex.EncodeToString(hash.Sum(nil))
 
+	fmt.Printf("----------------------------------------------\n")
+	fmt.Printf("---------------- Marvel Heros ----------------\n")
+	fmt.Printf("----------------------------------------------\n")
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Printf("Bienvenido al servicio de consulta de superheroes\n")
+	fmt.Printf("Elija una opción\n")
+	fmt.Printf("1. Buscar superheroe por nombre\n")
+	fmt.Printf("2. Listar los últimos 20 registros\n")
+
+	opt, _ := reader.ReadString('\n')
+
+	switch opt {
+	case "1":
+
+	case "2":
+
+
+	}
+
+
 	URL := "http://gateway.marvel.com/v1/public/characters?limit=100&ts="+ ts +"&apikey="+ publicKey +"&hash=" + myHash
-	fmt.Println(URL)
 	response, err := http.Get(URL)
 
 	if err != nil {
@@ -56,22 +77,12 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	//fmt.Println(string(responseData))
 
-	//var responseObject Response
 	responseObject := Response{}
 	json.Unmarshal(responseData, &responseObject)
-	//json.NewDecoder(response.Body).Decode(&responseObject)
-	//fmt.Println(responseObject)
 
 	fmt.Println(responseObject.Data.Results)
-	//fmt.Println(responseObject.Status)
-	/*
 
-	//time := time.Now();
-	fmt.Printf("%s \n",myHash);
-	fmt.Println(URL);
-	fmt.Printf(ts);*/
 
 
 
