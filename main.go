73d6b4756c5efd49031eb4a3011ab77733b04bcf
unknown <jose.salazar1@unmsm.edu.pe>
2019-02-02 02:24:33 -0500

    Menu(done)

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"crypto/md5"
 	"encoding/hex"
 	"encoding/json"
@@ -16,20 +15,50 @@ import (
 
 
 type Response struct {
-	Code  int `json:"code"`
-	Status string `json:"status"`
+	//Code  int `json:"code"`
+	//Status string `json:"status"`
 	Data Data `json:"data"`
 }
 
 type Data struct {
 	Results []Results `json:"results"`
+	Count 	int		  `json:"count"`
 }
 
 type Results struct {
 	Id int `json:"id"`
 	Name string `json:"name"`
 	Desc string `json:"description"`
+	Comics []Comics `json:"comics"`
 }
+
+type Comics struct {
+	Available int  `json:"available"`
+	Items []Items `json:"items"`
+}
+
+type Items struct {
+	ResourceURI 	string	`json:"resourceURI"`
+	Name 	string		`json:"name"`
+}
+
+type ComicResponse struct {
+	Data Data `json:"data"`
+}
+
+type ComicData struct {
+	Results []Results `json:"results"`
+	Count 	int		  `json:"count"`
+}
+
+type ComicResults struct {
+	Id int `json:"id"`
+	Name string `json:"name"`
+	Desc string `json:"description"`
+	Comics []Comics `json:"comics"`
+}
+
+
 
 func main() {
 	const privateKey = "eb6918567366c3ce1ef3c492b0d8d866e913e7cd"
@@ -44,44 +73,60 @@ func main() {
 	hash.Write([]byte(par))
 	myHash := hex.EncodeToString(hash.Sum(nil))
 
-	fmt.Printf("----------------------------------------------\n")
-	fmt.Printf("---------------- Marvel Heros ----------------\n")
-	fmt.Printf("----------------------------------------------\n")
+	fmt.Printf("----------------------------------------------------\n")
+	fmt.Printf("------------------- Marvel Heros -------------------\n")
+	fmt.Printf("----------------------------------------------------\n")
 
-	reader := bufio.NewReader(os.Stdin)
+
 	fmt.Printf("Bienvenido al servicio de consulta de superheroes\n")
 	fmt.Printf("Elija una opción\n")
 	fmt.Printf("1. Buscar superheroe por nombre\n")
 	fmt.Printf("2. Listar los últimos 20 registros\n")
 
-	opt, _ := reader.ReadString('\n')
-
-	switch opt {
-	case "1":
-
-	case "2":
-
-
-	}
-
-
-	URL := "http://gateway.marvel.com/v1/public/characters?limit=100&ts="+ ts +"&apikey="+ publicKey +"&hash=" + myHash
-	response, err := http.Get(URL)
-
+	var opt int
+	_,err := fmt.Scan(&opt)
 	if err != nil {
 		fmt.Print(err.Error())
-		os.Exit(1)
 	}
-	defer response.Body.Close()
-	responseData, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		log.Fatal(err)
+	switch opt {
+	case 1:
+		break
+	case 2:
+		URL := "http://gateway.marvel.com/v1/public/characters?limit=20&ts="+ ts +"&apikey="+ publicKey +"&hash=" + myHash
+		response, err := http.Get(URL)
+		//fmt.Println(URL)
+		if err != nil {
+			fmt.Print(err.Error())
+			os.Exit(1)
+		}
+		defer response.Body.Close()
+		responseData, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		responseObject := Response{}
+		json.Unmarshal(responseData, &responseObject)
+
+		for i:=0;i< responseObject.Data.Count ;i++  {
+			fmt.Print("Resultado Nº ",(i+1),"\n")
+			fmt.Println("ID:")
+			fmt.Println(responseObject.Data.Results[i].Id)
+			fmt.Println("Nombre:")
+			fmt.Println(responseObject.Data.Results[i].Name)
+			fmt.Println("Descripción:")
+			if responseObject.Data.Results[i].Desc == "" {
+				fmt.Println("No hay información disponible")
+			}else {
+				fmt.Println(responseObject.Data.Results[i].Desc)
+			}
+			fmt.Println("-------------------------------------")
+		}
+		break
 	}
 
-	responseObject := Response{}
-	json.Unmarshal(responseData, &responseObject)
 
-	fmt.Println(responseObject.Data.Results)
+
 
 
 
